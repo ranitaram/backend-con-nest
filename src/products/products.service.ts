eb2faa195c9d37aaba1af84aa1e0622e3ed0c04b
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2022-10-14 09:03:24 -0600

    manejo de errores

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -7,6 +7,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
   
   constructor(
     @InjectRepository(Product)
@@ -24,8 +26,7 @@ export class ProductsService {
       return product;
     
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException('Ayuda')
+      this.handleDBExceptions(error);
     }
   }
 
@@ -43,5 +44,17 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExceptions(error: any){
+    if (error.code === '23505') 
+    throw new BadRequestException(error.detail);
+
+    this.logger.error(error)
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs'
+    );
+      
+    
   }
 }
